86e0e4721e3eb42696e9d7e98e25bf30c7bb5440
Ruofan Wu <r8wu@ucsd.edu>
2025-05-21 13:03:39 -0700

    “msiapexmine”

--- exa1.cc
+++ exa1.cc
@@ -1,9 +1,9 @@
-#@title strain series
-S1 = ['mono','cyclic']
-S2 = ['fixed_peak_points']
-S3 = [1]
-num_samples = 500
-bb = 0.05
-
-strain_history = []
-
+template <int dim>
+struct PointHistory
+{
+//	J2Material material_point = J2Material(/*H_=*/2/3 * 4, /*beta_=*/1.0, /*sy=*/1.2, /*E_=*/, /*nu_=*/0.3);
+	double EEE = 50.0 / 3.0 / (1 - 2 * 0.3);
+	DruckerPrager material_point = DruckerPrager(0.3, EEE, 30, 20000000000000, 10, 20);
+//      Point<dim> point;
+	double strain_xx;
+		double strain_yy;
